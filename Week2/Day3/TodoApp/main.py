8604e4221a530a2c30f96ae0fe0e44553e6cc9db
Reddy Harsha Sai Venkata Prudhvi <prudhvi.bablu04@gmail.com>
2026-04-15 11:18:18 -0700

    13-04-2026

--- Week2/Day3/TodoApp/main.py
+++ Week2/Day3/TodoApp/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-import models
+import model
 from database import engine
 from routers import auth, todos, admin, users
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+model.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
