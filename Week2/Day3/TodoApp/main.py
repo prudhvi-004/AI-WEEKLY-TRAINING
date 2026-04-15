79a006dfd640c647bbafb06f2e2d972a731a76d3
Reddy Harsha Sai Venkata Prudhvi <prudhvi.bablu04@gmail.com>
2026-04-15 03:44:14 -0700

    13-04-2026

--- Week2/Day3/TodoApp/main.py
+++ Week2/Day3/TodoApp/main.py
@@ -1,22 +1,11 @@
-# Import FastAPI framework → used to create APIs
 from fastapi import FastAPI
-
-# Import your models file (where you defined tables like Todo, User, etc.)
-import model
-
-# Import engine (DB connection) from your database.py file
+import models
 from database import engine
+from routers import auth, todos, admin, users
 
-
-# Create FastAPI app instance
-# This is the main entry point of your API
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
 
-# Base → comes from declarative_base() in database.py
-# metadata → stores info about all tables you created (like Todo table, etc.)
-
-# create_all() → creates tables in the database
-# bind=engine → tells SQLAlchemy WHICH database to create tables in
-
-model.Base.metadata.create_all(bind=engine)
+app.include_router(auth.router)
+app.include_router(todos.router)
